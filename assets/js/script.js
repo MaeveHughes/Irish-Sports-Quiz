c59078d341a6ce4f1e4f66a12331152ceee6520a
MaeveHughes <maevehughes@gmail.com>
2021-08-16 14:46:31 +0000

    Adjusting the line breaks on line 76 to be consistent with the other line breaks after each og the quis questions. Adding a button to the bottom of the questions so users can submit their answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/** Checks the answer and confirms if the answers are correct or incorrect */
 function check () {
     var score = 0
     var correct_answer_1 = document.getElementById('1answer3');
@@ -6,7 +7,7 @@ function check () {
     var question_one_answer_four = document.getElementById('1answer4');
     if (correct_answer_1.checked === true) {
         score++
-        alert('You are a genius... you got the first question right!)
+        alert('You are a genius... you got the first question right!')
     } else {
         alert('Oh no... you got the first question wrong!')
     }
