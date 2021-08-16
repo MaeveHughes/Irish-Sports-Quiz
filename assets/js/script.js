1b4093c1ec6a43af0bdfad0eaefe8e4f603850ba
MaeveHughes <maevehughes@gmail.com>
2021-08-16 14:40:35 +0000

    Adding in the javascipt for question one to confirm to the user if there answer is correct or incorrect

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function check () {
+    var score = 0
+    var correct_answer_1 = document.getElementById('1answer3');
+    var question_one_answer_one = document.getElementById('1answer1');
+    var question_one_answer_two = document.getElementById('1answer2');
+    var question_one_answer_four = document.getElementById('1answer4');
+    if (correct_answer_1.checked === true) {
+        score++
+        alert('You are a genius... you got the first question right!)
+    } else {
+        alert('Oh no... you got the first question wrong!')
+    }
+}
